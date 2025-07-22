bed8ad6988955b47c4b0882651fd1b6213a839bb
Shon Joseph <70068599+shon6190@users.noreply.github.com>
2025-07-22 16:56:47 +0530

    Update create-iconcss.php

--- functions/create-iconcss.php
+++ functions/create-iconcss.php
@@ -5,6 +5,9 @@ function icon_upload($file, $name)
     $fileName = $file['name'];
     $targetPath = 'uploads/';
     $targetPath = SC_CORE_PATH . 'widget-icons/icons/';
+    if (!file_exists($targetPath)) {
+        mkdir($targetPath, 0777, true);
+    }
     $new_file_name = $name . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
     // $targetFile = $targetPath . $fileName;
     if (move_uploaded_file($tempFile, $targetPath . $new_file_name)) {
